2d5070cb06c3afb5cd5b8dc8447fd246f36255f5
cesarleyton <cesarleyton549@gmail.com>
2024-06-02 10:25:12 -0500

    fix

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -13,7 +13,7 @@ export const lucia = new Lucia(adapter, {
     expires: false,
 
     attributes: {
-      sameSite: "none",
+      sameSite: "None",
 
       secure: process.env.NODE_ENV !== "development", // set `Secure` flag in HTTPS
     },
