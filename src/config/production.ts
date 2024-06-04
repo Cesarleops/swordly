f6beade40c779baba98500e6d205c47b03362942
cesarleyton <cesarleyton549@gmail.com>
2024-06-04 11:22:54 -0500

    updated front url

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,5 +1,5 @@
 export const production = {
-  clientUrl: "https://swordly-front.vercel.app",
+  clientUrl: "https://swordlyfront-production.up.railway.app",
   dbUrl: process.env.DB_URL,
   PORT: process.env.PORT,
   serverUrl: "https://swordly-production.up.railway.app",
