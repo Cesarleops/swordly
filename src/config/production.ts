fd6d02809ca80400f98c7af20014782f32e3d97a
cesarleyton <cesarleyton549@gmail.com>
2024-06-04 09:09:05 -0500

    added new deploy url

--- src/config/production.ts
+++ src/config/production.ts
@@ -2,5 +2,5 @@ export const production = {
   clientUrl: "https://swordly-front.vercel.app",
   dbUrl: process.env.DB_URL,
   PORT: process.env.PORT,
-  serverUrl: "https://swordly.onrender.com",
+  serverUrl: "https://swordly-production.up.railway.app",
 };
